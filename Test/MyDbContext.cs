ef2f010728a7d906cdd505417e18410b8bdf0852
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2021-08-06 08:00:35 +0200

    wrote test using InMemory provider
    works

---
+++ Test/MyDbContext.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Test
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Assessment> Assessments { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseInMemoryDatabase("TestDb");
+    }
+
+    public class Assessment
+    {
+        public int Id { get; set; }
+        public Contract Contract { get; set; } = null!;
+    }
+
+    public class Contract
+    {
+        public int Id { get; set; }
+        public ICollection<Company> Companies { get; set; } = null!;
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public ICollection<Signatory> Signatories { get; set; } = null!;
+    }
+
+    public class Signatory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace Test
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // setup
+            using (var db = new MyDbContext())
+            {
+                var assessment = new Assessment
+                {
+                    Contract = new Contract
+                    {
+                        Companies = new[]
+                        {
+                            new Company
+                            {
+                                Signatories = new[]
+                                {
+                                    new Signatory
+                                    {
+                                        Name = "Foo"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+                db.Add(assessment);
+                db.SaveChanges();
+            }
+
+            // tests
+            using (var db = new MyDbContext())
+            {
+                var assessment = db.Assessments.Single();
+                if (assessment.Contract != null) throw new Exception("We did not include Contract");
+                assessment = db.Assessments
+                    .Include(x => x.Contract.Companies)
+                        .ThenInclude(x => x.Signatories)
+                    .Single();
+                if (assessment.Contract == null) throw new Exception("We included Contract");
+                if (assessment.Contract.Companies == null) throw new Exception("We included Companies");
+                foreach (var company in assessment.Contract.Companies)
+                {
+                    if (company.Signatories == null) throw new Exception("We included Signatories");
+                }
+                Console.WriteLine("All good");
+            }
         }
     }
 }
